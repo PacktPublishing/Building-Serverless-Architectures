c17673cc1671d68bf3a1fbd843bc22585740185e
Dhiraj Chandanshive <dhirajc@packtpub.com>
2017-07-14 16:53:05 +0530

    Add files via upload

---
+++ services-user/src/main/java/com/serverlessbook/services/user/domain/Token.java
@@ -0,0 +1,29 @@
+package com.serverlessbook.services.user.domain;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+
+public class Token {
+
+    @DynamoDBHashKey(attributeName = "Token")
+    private String token;
+
+    @DynamoDBAttribute(attributeName = "UserId")
+    private String userId;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+}
